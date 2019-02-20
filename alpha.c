2e71e40a9a69b6796343c72979ff60830b6e6f26
amrithadevi <sangeethameganathan98@gmail.com>
2019-02-20 14:27:15 +0530

    Update alpha.c

--- alpha.c
+++ alpha.c
@@ -4,7 +4,7 @@ void main()
 {
  char a;
  clrscr();
-  if(a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h'||a=='i'||a=='j'||a=='k'||a=='l'||a=='m'||a=='n'||a=='o'||a=='p'||a=='q'||a=='r'||a=='s'||a=='t'||a=='u'||a=='v'||a=='w'||a=='x'||a=='y'||a=='z')
+  if(a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h'||a=='i'||a=='j'||a=='k'||a=='l'||a=='m'||a=='n'||a=='o'||a=='p'||a=='q'||a=='r'||a=='s'||a=='t'||a=='u'||a=='v'||a=='w'||a=='x'||a=='y'||a=='z'||a=='Z')
     print("Alphabet");
   else
     print("No");
