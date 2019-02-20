887e07508d2a439ca1e0bc141e92c92cf2bef512
amrithadevi <sangeethameganathan98@gmail.com>
2019-02-20 14:24:38 +0530

    Update alpha.c

--- alpha.c
+++ alpha.c
@@ -1,7 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
 void main()
 {
  char a;
-  if(a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h'||a=='i'||a=='j'||a=='k'||a=='l')
+ clrscr();
+  if(a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h'||a=='i'||a=='j'||a=='k'||a=='l'||a=='m'||a=='n'||a=='o'||a=='p'||a=='q'||a=='r'||a=='s'||a=='t'||a=='u'||a=='v'||a=='w'||a=='x'||a=='y'||a=='z')
     print("alphabet");
   else
     print("not a alphbet");
