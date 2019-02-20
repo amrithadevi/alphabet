7999105f39ecbb56cdf0b8669e7623ac06267afb
amrithadevi <sangeethameganathan98@gmail.com>
2019-02-20 14:00:04 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,9 @@
+void main()
+{
+ char a;
+  if(a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h'||a=='i'||a=='j'||a=='k'||a=='l')
+    print("alphabet");
+  else
+    print("not a alphbet");
+  getch();
+ } 
